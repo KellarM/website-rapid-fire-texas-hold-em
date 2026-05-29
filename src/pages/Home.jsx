083863147821ad3352db2d9172bde835835fd8b4
KellarM <Kellar_em@yahoo.ca>
2026-05-29 11:34:08 -0600

    feat: insert StadiumSection between OverviewSection and CascadeSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import NavBar from '../components/NavBar';
 import HeroSection from '../components/HeroSection';
 import OverviewSection from '../components/OverviewSection';
+import StadiumSection from '../components/StadiumSection';
 import CascadeSection from '../components/CascadeSection';
 import TechnologySection from '../components/TechnologySection';
 import CompetitiveSection from '../components/CompetitiveSection';
@@ -10,15 +11,13 @@ import AboutSection from '../components/AboutSection';
 import ContactSection from '../components/ContactSection';
 import Footer from '../components/Footer';
 
-// NOTE: GallerySection has been removed from this page intentionally.
-// The GallerySection.jsx component file can be deleted from /src/components/
-
 export default function Home() {
   return (
     <div style={{ minHeight: '100vh', backgroundColor: '#0A0A0A' }}>
       <NavBar />
       <HeroSection />
       <OverviewSection />
+      <StadiumSection />
       <CascadeSection />
       <TechnologySection />
       <CompetitiveSection />
